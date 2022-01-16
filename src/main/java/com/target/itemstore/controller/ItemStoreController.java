30f115fadf6fbe1c636da47b659450dd85ee71c4
Ais <ais@home.local>
2022-01-15 19:15:47 -0600

    Modify response of GetItems Endpoint

--- src/main/java/com/target/itemstore/controller/ItemStoreController.java
+++ src/main/java/com/target/itemstore/controller/ItemStoreController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ItemStoreController {
     @GetMapping("/items")
     String getItems() {
-        return "Hello";
+        return "Hello World";
     }
 }
